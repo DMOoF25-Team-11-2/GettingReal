a6100d091c7cfff9d4a629ecf2101d3bb5e27230
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-05-06 12:20:36 +0800

    Change Box repos xml to save guid for material

--- GettingReal/Model/Box.cs
+++ GettingReal/Model/Box.cs
@@ -1,22 +1,81 @@
 ﻿namespace GettingReal.Model;
+using System.Xml.Serialization;
 
+/// <summary>
+/// Represents a box that can contain materials.
+/// </summary>
 public class Box
 {
+    /// <summary>
+    /// Unique identifier for the box.
+    /// </summary>
     public Guid GUID { get; set; }
+    /// <summary>
+    /// Name of the box.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Description of the box.
+    /// </summary>
     public string Description { get; set; }
-    private List<Material> Materials { get; set; }
 
-    public Box()
+    /// <summary>
+    /// List of materials contained in the box.
+    /// </summary>
+    /// <remarks>
+    /// This property is used to store the materials that are contained in the box.
+    /// </remarks>
+    [XmlIgnore]
+    public List<Material> Materials { get; set; }
+
+    /// <summary>
+    /// List of GUIDs representing the materials in the box.
+    /// </summary>
+    /// <remarks>
+    /// This property is used for XML serialization and deserialization.
+    /// It converts the list of Material objects to a list of GUIDs.
+    /// </remarks>
+    [XmlArrayItem("MaterialGUID")]
+    public List<Guid> MaterialGUIDs
     {
-        GUID = Guid.NewGuid();
-        Materials = new List<Material>(); // Initialize Materials collection
+        get => Materials?.Select(m => m.GUID).ToList() ?? new List<Guid>();
+        set
+        {
+            if (Materials == null)
+                Materials = new List<Material>();
+            Materials.Clear();
+            if (value != null)
+            {
+                foreach (var guid in value)
+                {
+                    Materials.Add(new Material { GUID = guid });
+                }
+            }
+        }
     }
 
-    // Adding a public parameterless constructor to fix CS0310
-    public Box(string name = "", string description = "")
+    /// <summary>
+    /// Default constructor for XML serialization.
+    /// </summary>
+    /// <remarks>
+    /// Needs to be public to satisfy the XML serialization requirements.
+    /// </remarks>
+    public Box() : this(string.Empty, string.Empty)
     {
-        Materials = new List<Material>(); // Initialize Materials collection
+        GUID = Guid.NewGuid();
+    }
+
+    /// <summary>
+    /// Constructor to create a Box with a name and description.
+    /// </summary>
+    /// <param name="name">The name of the box.</param>
+    /// <param name="description">The description of the box.</param>
+    /// <remarks>
+    /// This constructor is used to create a new Box instance with a specified name and description.
+    /// </remarks>
+    public Box(string name = "", string description = "") : base()
+    {
+        GUID = Guid.NewGuid();
         Name = name;
         Description = description;
     }
--- GettingReal/Model/Material.cs
+++ GettingReal/Model/Material.cs
@@ -2,7 +2,7 @@
 
 public class Material
 {
-    public Guid GUID { get; private set; }
+    public Guid GUID { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public int Amount { get; set; }
@@ -11,5 +11,8 @@ public class Material
     public Material()
     {
         GUID = Guid.NewGuid();
+        Name = string.Empty;
+        Description = string.Empty;
+        Amount = 0;
     }
 }
