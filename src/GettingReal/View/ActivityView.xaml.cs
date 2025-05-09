d351c88833854691a00b629f52e651f8993ffab2
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-05-09 11:50:08 +0800

    Add Activity MVVM

--- src/GettingReal/View/ActivityView.xaml.cs
+++ src/GettingReal/View/ActivityView.xaml.cs
@@ -15,4 +15,12 @@ public partial class ActivityView : Page
         InitializeComponent();
         DataContext = new ActivityViewModel();
     }
+    private void ListViewActivities_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        if (e.Key == System.Windows.Input.Key.Escape)
+        {
+            ListViewActivities.SelectedItem = null;
+            e.Handled = true; // Prevent further processing of the Escape key
+        }
+    }
 }
