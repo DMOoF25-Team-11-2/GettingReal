51c8f63806e9b3a8f95aa5265581d77a8624c58d
Cecilie <ceciliek1989@outlook.com>
2025-05-19 12:21:35 +0200

    Reportgen with foreach

--- src/GettingReal/Handler/ReportGenerator.cs
+++ src/GettingReal/Handler/ReportGenerator.cs
@@ -17,7 +17,7 @@ public class ReportGenerator //generates reports in Xaml-format
         BoxRepository boxes = new();
 
         xamlBuilder.AppendLine("<StackPanel xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>");
-
+    
         foreach (Guid guid in workshop.ActivityGuids)
         {
             Activity? activity = new ActivityRepository().Get(guid);
@@ -30,9 +30,11 @@ public class ReportGenerator //generates reports in Xaml-format
                     {
                         if (materialGuidInBox == materialGuidInActivity)
                         {
+                            
                             Material? material = materials.Get(materialGuidInActivity);
+                            xamlBuilder.AppendLine($"   <TextBlock Text='{box.Name}' FontSize='16' />");
                             xamlBuilder.AppendLine($"   <TextBlock Text='{material.Name}' FontSize='16' />");
-                            xamlBuilder.AppendLine($"   <TextBlock Text='{material.Description}' FontSize='12' Foreground='Gray' />");
+                            xamlBuilder.AppendLine($"   <TextBlock Text='{material.Description}' FontSize='16' Foreground='Gray' />");
 
                         }
                     }
