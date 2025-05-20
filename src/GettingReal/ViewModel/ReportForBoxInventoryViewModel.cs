6a81bed84c9e719d76ec651469cb32124dd6d18c
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-05-20 16:50:00 +0800

    Add report for inventory in box

--- src/GettingReal/ViewModel/ReportForBoxInventoryViewModel.cs
+++ src/GettingReal/ViewModel/ReportForBoxInventoryViewModel.cs
@@ -39,7 +39,6 @@ public class ReportForBoxInventoryViewModel : ViewModelBase
     #endregion
 
     #region Commands properties
-    public RelayCommand ExportToPdfCommand { get; private set; }
     public RelayCommand PrintReportCommand { get; private set; }
     #endregion
 
@@ -50,7 +49,6 @@ public class ReportForBoxInventoryViewModel : ViewModelBase
 
         Boxes = new ObservableCollection<Box>(_boxRepository.GetAll());
 
-        ExportToPdfCommand = new RelayCommand(ExecuteExportToPdf, CanExecuteExportToPdf);
         PrintReportCommand = new RelayCommand(ExecutePrintReport, CanExecutePrintReport);
     }
 
@@ -67,31 +65,23 @@ public class ReportForBoxInventoryViewModel : ViewModelBase
             if (material != null)
                 materials.Add(material);
         }
-
-        MaterialsInBox = new ObservableCollection<Material>(materials);
+        MaterialsInBox = [.. materials];
     }
 
     #region Buttons action
-    private void ExecuteExportToPdf()
-    {
-        string report = ReportGenerator.ReportBoxInventory(_selectedBox);
-        ReportGenerator.Print(report);
-    }
-
     private void ExecutePrintReport()
     {
+        string report = ReportGenerator.ReportBoxInventory(_selectedBox!);
+        ReportGenerator.Print(report);
     }
     #endregion
 
     #region Button condition
-    private bool CanExecuteExportToPdf()
-    {
-        return SelectedBox != null;
-    }
 
     private bool CanExecutePrintReport()
     {
-        return SelectedBox != null;
+        return true;
+        return _selectedBox != null;
     }
     #endregion
 }
